60936393cfdc78f179ab561ba708205c51b52402
booknap <napasai45@gmail.com>
2017-10-08 23:01:23 +0700

    refactor

--- src/code/Restaurant.java
+++ src/code/Restaurant.java
@@ -14,15 +14,33 @@ public class Restaurant {
 
 	public static Scanner sc = new Scanner(System.in);
 
-	public static int calculateTotalPrice(){
+	public static void printChange(int cha) {
+		System.out.printf("\nYour change is %d Baht.\n", cha);
+		System.out.print("+----------------------+------+\n");
+		System.out.print("| Banknotes or coins   | Qty  |\n");
+		System.out.print("+----------------------+------+\n");
+		calculateChange(cha);
+		System.out.print("+----------------------+------+\n");
+	}
+
+	public static void calculateChange(int ch) {
+		for (int j = 0; j < bankAndCoin.length; j++) {
+			if (ch / bankAndCoin[j] != 0) {
+				System.out.printf("| %-4d Baht banknotes  |%5d |\n", bankAndCoin[j], ch / bankAndCoin[j]);
+			}
+			ch = ch % bankAndCoin[j];
+		}
+	}
+
+	public static int calculateTotalPrice() {
 		int totalPrice = 0;
 		for (int i = 0; i < menu.length; i++) {
 			totalPrice += price[i];
 		}
-		
+
 		return totalPrice;
 	}
-	
+
 	public static void createTable() {
 		System.out.print("+------ Menu ------+-- Qty --+-- Price --+\n");
 		for (int i = 0; i < menu.length; i++) {
@@ -30,15 +48,16 @@ public class Restaurant {
 				System.out.printf("| %-17s|%9d|%11d|\n", menu[i], totalQuantity[i], price[i]);
 			}
 		}
-		
+
 		System.out.print("+------------------+---------+-----------+\n");
-		System.out.printf("|%-28s|%11d|\n", " Total", calculateTotalPrice() );
+		System.out.printf("|%-28s|%11d|\n", " Total", calculateTotalPrice());
 		System.out.print("+------------------+---------+-----------+\n");
 	}
-	
+
 	static int quantity = 0;
-	static String[] menu = { "Pizza", "Chicken", "Coke" };
-	static int[] productPrice = { 250, 120, 45 };
+	static String[] menu = { "Pizza", "Spaghetti", "Chicken", "Coke" };
+	static int[] productPrice = { 250, 150, 120, 45 };
+	static int[] bankAndCoin = { 1000, 500, 100, 50, 20, 10, 5, 2, 1 };
 	static int[] totalQuantity = new int[menu.length];
 	static int[] price = new int[menu.length];
 
@@ -53,22 +72,29 @@ public class Restaurant {
 		for (int x = 0; x < menu.length; x++) {
 			System.out.printf("%d.) %s\t\t%d Baht.\n", x + 1, menu[x], productPrice[x]);
 		}
-		
-		System.out.printf("4.) Total\n5.) Exit\n");
-		
+
+		System.out.printf("%d.) Total\n%d.) Exit\n", menu.length + 1, menu.length + 2);
+
 		int choice = 0;
 
 		while (true) {
 			System.out.print("\nEnter your Choice: ");
 			choice = sc.nextInt();
-			if (choice == 5) {
+			if (choice == menu.length + 2) {
+				System.out.print("\nInput your Cash: ");
+				int cash = sc.nextInt();
+				int change = cash - calculateTotalPrice();
+				if (change < 0) {
+					System.out.print("Not enough cash.");
+					break;
+				} else {
+					printChange(change);
+				}
 				System.out.print("\n========== Thank you ==========");
 				break;
-			}
-			else if (choice == 4) {
+			} else if (choice == menu.length + 1) {
 				createTable();
-			}
-			else if (choice == 1||choice == 2||choice == 3) {
+			} else if (choice >= 1 && choice<=menu.length) {
 				System.out.print("Enter quantity: ");
 				getPrice(choice);
 			}
